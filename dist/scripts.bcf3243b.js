2a0d9ab134641dcbfff68181aa9af3aeb22fe031
polina090193 <polina_0901@mail.ru>
2021-10-18 21:47:24 +0300

    Fix the problem with images

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -357,7 +357,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55790" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60205" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
